59fc210e6b2da4636c93341938a05a144d48c4bc
Demo User <debian@beaglebone.localdomain>
2017-10-27 16:23:44 +0000

    adding json parse file and result text file

---
+++ boggle/jsonparse.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+import json
+from pprint import pprint
+
+with open('wordpuzzle.json') as data_file:
+	file=data_file.read()
+	data=json.loads(file)
+
+file=open("output.txt","w")
+
+
+file.write(data['textAnnotations'][0]['description'])
+file.close()
